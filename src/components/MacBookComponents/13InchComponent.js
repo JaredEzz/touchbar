8b5ccae02a70c42de9b8c69fba96f4646338bb7b
Jared Hasson <jhasson@simplifile.com>
2019-02-23 18:32:43 -0700

    added links to details. price comparison feature is complete.

--- src/components/MacBookComponents/13InchComponent.js
+++ src/components/MacBookComponents/13InchComponent.js
@@ -14,9 +14,11 @@ const Thirteen = () => {
                 transform: 'translate(-50%, 0%)'
             }}>
             <h3 className="display-3">13-inch MacBook Pro Models</h3>
+            <a href="https://www.apple.com/hk/shop/buy-mac/macbook-pro/13-inch">
             <img src={"https://raw.githubusercontent.com/JaredEzz/touchbar/master/res/thirteendetails.png"}
                  width="1159.7" height="649.6"
-                 alt={"13 inch macbook options and prices"}/>
+                 alt={"13 inch macbook options and prices"}
+                 href="https://www.apple.com/hk/shop/buy-mac/macbook-pro/13-inch"/></a>
             <div className="left">
                 <Link className="header_link" to={"/compare/fifteen"}>See 15-inch Models</Link>
             </div>
--- src/components/MacBookComponents/15InchComponent.js
+++ src/components/MacBookComponents/15InchComponent.js
@@ -15,9 +15,10 @@ const Fifteen = () => {
                 transform: 'translate(-50%, 0%)'
             }}>
             <h3 className="display-3">15-inch MacBook Pro Models</h3>
+            <a href="https://www.apple.com/hk/shop/buy-mac/macbook-pro/13-inch">
             <img src={"https://raw.githubusercontent.com/JaredEzz/touchbar/master/res/fifteendetails.png"}
                  width="900" height="649.6"
-                 alt={"15 inch macbook options and prices"}/>
+                 alt={"15 inch macbook options and prices"}/></a>
             <div className="left">
                 <Link className="header_link" to={"/compare/thirteen"}>See 13-inch Models</Link>
             </div>
